cf1dcf6dfd1cbce9d7a1ab9ec01e97c75ca5e4fb
heinlja <jaanus.heinlaid@gmail.com>
2014-11-04 19:03:21 +0200

    Continue with look, feel and translations for Estonian portal

--- ckanext/dgu/lib/helpers.py
+++ ckanext/dgu/lib/helpers.py
@@ -694,6 +694,10 @@ def get_package_fields(package, pkg_extras, dataset_type):
     # work out the dataset_type
     pkg_extras = dict(pkg_extras)
     harvest_date = harvest_guid = harvest_url = dataset_reference_date = None
+
+    # mandate field considered irrelevant for Estonian data portal.
+    field_names.remove(['mandate'])
+
     if dataset_type == 'uklp':
         field_names.add(['harvest-url', 'harvest-date', 'harvest-guid', 'bbox', 'spatial-reference-system', 'metadata-date', 'dataset-reference-date', 'frequency-of-update', 'responsible-party', 'access_constraints', 'metadata-language', 'resource-type'])
         field_names.remove(['geographic_coverage', 'mandate'])
