infoaed/ckanext-dgu

commit 123fcab1650e227ca540d29ef570589c4277d2e0

Preparations for allowing file upload in the dataset registration form + a bit other stuff.

ckanext/dgu/forms/dataset_form.py
@@ -40,12 +40,10 @@ geographic_granularity = [('', ''),
                           ('other', 'other - please specify')]
 
 update_frequency = [('', ''),
-                    ('never', 'never'),
-                    ('discontinued', 'discontinued'),
-                    ('annual', 'annual'),
-                    ('quarterly', 'quarterly'),
-                    ('monthly', 'monthly'),
-                    ('other', 'other - please specify')]
+                    ('annual', 'kord aastas'),
+                    ('quarterly', 'kord kvartalis'),
+                    ('monthly', 'kord kuus'),
+                    ('other', 'muu - palun täpsusta')]
 
 temporal_granularity = [("", ""),
                         ("year", "year"),

ckanext/dgu/drupalclient.py
@@ -56,8 +56,6 @@ class DrupalClient(object):
             if config.get('dgu.xmlrpc_https'):
                 xmlrpc_url_template = 'https://%s/services/xmlrpc'
 
-            log.info('-----------------------------------------------------  xmlrpc_url_template = %s', xmlrpc_url_template)
-
             xmlrpc_url = xmlrpc_url_template % server
             xmlrpc_url_log_safe = xmlrpc_url_template % server_log_safe
         log.info('XMLRPC connection to %s', xmlrpc_url_log_safe)

ckanext/dgu/schema.py
@@ -143,14 +143,11 @@ organisation_name_mapping = {
     'Cancer Registry Northern Ireland':'Northern Ireland Cancer Registry',
     }
 
-#update_frequency_options = ['never', 'discontinued', 'annual', 'quarterly', 'monthly']
-update_frequency_options = ['mitte kunagi', 'lõpetatud', 'iga-aastane', 'kvartaalne', 'igakuine']
+update_frequency_options = ['never', 'discontinued', 'annual', 'quarterly', 'monthly']
 
-#geographic_granularity_options = ['national', 'regional', 'local authority', 'ward', 'point']
-geographic_granularity_options = ['riiklik', 'regionaalne', 'kov']
+geographic_granularity_options = ['national', 'regional', 'local authority', 'ward', 'point']
 
-#temporal_granularity_options = ['year', 'quarter', 'month', 'week', 'day', 'hour', 'point']
-temporal_granularity_options = ['aasta', 'kvartal', 'kuu', 'nädal', 'päev', 'tund']
+temporal_granularity_options = ['year', 'quarter', 'month', 'week', 'day', 'hour', 'point']
 
 #category_options = ['Agriculture and Environment', 'Business and Energy', 'Children, Education and Skills', 'Crime and Justice', 'Economy', 'Government', 'Health and Social Care', 'Labour Market', 'People and Places', 'Population', 'Travel and Transport', 'Equality and Diversity', 'Migration']
 category_options = ['Põllumajandus, kalandus, metsandus', 'Kultuur', 'Majandus ja tööstus', 'Tööhõive', 'Haridus ja teadus', 'Keskkond', 'Rahandus ja eelarvestamine', 'Geograafia', 'Tervis', 'Riigiteenused', 'Avatud valitsemine', 'Rahvastiku andmed', 'Transport', 'Sotsiaalvaldkond']

ckanext/dgu/theme/templates/package/edit_form.html
@@ -4,6 +4,8 @@
   xmlns:py="http://genshi.edgewall.org/"
   xmlns:xi="http://www.w3.org/2001/XInclude"
   role="form"
+  data-module="basic-form resource-form"
+  enctype="multipart/form-data"
 >
 
 <xi:include href="../_dgu_util.html" />
@@ -287,12 +289,6 @@
                        type="text"
                        value="${res.get(col, '')}"
                        class="form-control"/>
-                <br/>
-                <input id="individual_resources__${num}__upload"
-                       name="individual_resources__${num}__upload"
-                       type="file"
-                       value=""
-                       class="form-control"/>
                 <span class="input-group-btn">
                   <button id="individual_resources__${num}__validate-resource-button"
                       class="validate-resource-button btn btn-default">Check</button>
@@ -327,6 +323,35 @@
       <button class="btn-default validate-resources-button btn" id="validate-timeseries-button">Kontrolli kõike URLe...</button>
       <span class="checking-links-label" style="display: none;">Kontrollin, palun oodake...</span><br/>
     </fieldset>
+    <hr/>
+
+    <div id="file-upload-div">
+        <p>
+            <strong>Andmehulka saab üles laadida ka faili(de)na</strong>, kasutades alljärgnevaid sisendeid.<br/>
+            Nad võivad olla kas lisa ülaltoodud linkidele, kuid võite ka piirduda ainult üle laetud failidega.<br/>
+            Samas on suuri andmekogusid otstarbekam serveeerida teabevaldaja lingi tagant, mistõttu üles laetavate failide suuruse piirang on ${config.get('ckan.max_resource_size', '10')} MB.<br/>
+            Te saate korraga üles laadida ühe faili. Uute failide laadimiseks peate antud vormile läbi andmehulga muutmise operatsiooni tagasi pöörduma.<br/>
+        </p>
+        <table class="table table-bordered table-condensed table-striped" id="file-upload-table">
+            <thead>
+                <tr>
+                    <th class="resource-name" style="width:30%"><div class="js-tooltip" title="" data-original-title="Tekst, mida kuvatakse andmehulga lingi kõrval">Faili pealkiri: <i class="icon-info-sign"></i></div></th>
+                    <th class="resource-url" style="width:50%"><div class="js-tooltip" title="" data-original-title="Tekst, mida kuvatakse andmehulga lingi kõrval">Lühikirjeldus: <i class="icon-info-sign"></i></div></th>
+                    <th class="resource-name" style="width:20%"><div class="js-tooltip" title="" data-original-title="Faili formaat">Faili formaat: <i class="icon-info-sign"></i></div></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="individual_resources__0 resource" style="background-color:#f9f9f9">
+                    <td class="resource-description"><input type="text" name="file_name" value="" class="form-control"/></td>
+                    <td class="resource-description"><input type="text" name="file_descr" value="" class="form-control"/></td>
+                    <td class="resource-format"><input type="text" name="file_format" value="" class="form-control"/></td>
+                </tr>
+                <tr class="individual_resources__1 resource">
+                    <td colspan="3"><label for="file-upload-input">Lae fail:</label>&nbsp;<input id="file-upload-input" type="file" name="file_uploaded" value="" style="display:inline"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <hr/>
 
     <p><strong>Formaat:</strong> Kirjeldab faili formaati. Soovitatavad väärtused:<br/>

ckanext/dgu/theme/templates/package/read.html
@@ -71,7 +71,7 @@
       <!--! id=dataset for ckanext-spatial -->
     </div>
 
-    <div class="dataset-resources" py:if="not h.is_unpublished_item(c.pkg_dict)">
+    <div class="dataset-resources">
       <!-- Resources -->
       <py:if test="h.individual_resources()">
         <h2>Andmefailid (${len(h.individual_resources())})</h2>

commit 33bd11ccbbeff39f95313462c16661b68b5259d5

Changes in footer, revert dataset_form.py because commit 123fcab1650e227ca540d29ef570589c4277d2e0 broke something in there, needs investigation.

ckanext/dgu/forms/dataset_form.py
@@ -1,6 +1,4 @@
-﻿# -*- coding: utf-8 -*-
-
-import re
+﻿import re
 import json
 
 from ckan.lib.base import c, model
@@ -42,10 +40,12 @@ geographic_granularity = [('', ''),
                           ('other', 'other - please specify')]
 
 update_frequency = [('', ''),
-                    ('annual', 'kord aastas'),
-                    ('quarterly', 'kord kvartalis'),
-                    ('monthly', 'kord kuus'),
-                    ('other', 'muu - palun täpsusta')]
+                    ('never', 'never'),
+                    ('discontinued', 'discontinued'),
+                    ('annual', 'annual'),
+                    ('quarterly', 'quarterly'),
+                    ('monthly', 'monthly'),
+                    ('other', 'other - please specify')]
 
 temporal_granularity = [("", ""),
                         ("year", "year"),

ckanext/dgu/theme/templates/layout_base.html
@@ -235,18 +235,9 @@ ${h.top_level_init()}
   <div class="footer">
     <footer role="contentinfo" class="container">
       <ul class="links">
-        <li class="menu-531 first"><a href="/about">Mis on opendata.riik.ee?</a></li>
-        <li class="menu-531 first"><a href="/faq">KKK</a></li>
-        <li class="menu-531 first"><a href="http://www.riso.ee/et/avaandmete-roheline-raamat">Avaandmete roheline raamat</a></li>
-        <li class="menu-520"><a href="mailto:opendatasupport@mkm.ee" title="">Kontakt</a></li>
-<!--         <li class="menu-527"><a href="/code-conduct">Code of conduct</a></li>
-        <li class="menu-524"><a href="/accessibility-statement">Accessibility</a></li>
-        <li class="menu-528"><a href="/cookies-policy">Cookies policy</a></li>
-        <li class="menu-537"><a href="/privacy">Privacy</a></li>
-        <li class="menu-536"><a href="/moderation-policy">Moderation policy</a></li> -->
-<!--       <li class="menu-538 "><a href="/terms-and-conditions">Kasutustingimused</a></li>
-        <li class="menu-538"><a href="/about">API</a></li>
-        <li class="menu-538 last"><a href="/about">Üldinfo</a></li> -->
+        <li class="menu-531 first"><a href="/about">Mis on avaandmete portaal?</a></li>
+        <li class="menu-538 "><a href="/terms-and-conditions">Kasutustingimused</a></li>
+        <li class="menu-538 last"><a href="mailto:opendatasupport@mkm.ee" title="">Kontakt</a></li>
       </ul>
       <div class="credits">
         <div class="credit">