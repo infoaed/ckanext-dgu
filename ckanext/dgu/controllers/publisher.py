8ec8fb0253505527fb85a38db28d69f89607b407
heinlja <jaanus.heinlaid@gmail.com>
2015-01-14 10:43:30 +0200

    When clicking Save on the publisher/users/% page, return to the same page. Also small button label correction.

--- ckanext/dgu/controllers/publisher.py
+++ ckanext/dgu/controllers/publisher.py
@@ -233,7 +233,7 @@ class PublisherController(OrganizationController):
         group_member_save(context, data_dict, 'users')
         model.Session.commit()
 
-        h.redirect_to('/publisher/%s' % str(group.name))
+        h.redirect_to('/publisher/users/%s' % str(group.name))
 
 
     def users(self, id, data=None, errors=None, error_summary=None):
